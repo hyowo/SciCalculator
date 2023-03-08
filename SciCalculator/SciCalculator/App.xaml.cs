cc30694994e959194941ac88a3b1d30d75f234e4
Opilane TTHK <opilane@tthk.ee>
2023-03-08 14:31:00 +0200

    Switch font and force windows to run in mobile view.

--- SciCalculator/SciCalculator/App.xaml.cs
+++ SciCalculator/SciCalculator/App.xaml.cs
@@ -1,11 +1,27 @@
-﻿namespace SciCalculator;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace SciCalculator;
+
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow.GetFromWindowId(windowId).Resize(new SizeInt32(540, 1000));
+		});
+#endif
+        MainPage = new AppShell() {};
 	}
 }
--- SciCalculator/SciCalculator/MainPage.xaml.cs
+++ SciCalculator/SciCalculator/MainPage.xaml.cs
@@ -12,11 +12,7 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		CounterBtn.Text = $"Clicked {count} " + (count == 1 ? "time." : "times.") ;
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
